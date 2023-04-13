ea372ab7dc66772d6447def6f847a63e01169b49
sacrwst <sacrwst@gmail.com>
2023-04-14 03:12:25 +0900

    ✨ 検証用リンク追加

--- src/components/ui/layout/DefaultLayout.tsx
+++ src/components/ui/layout/DefaultLayout.tsx
@@ -8,17 +8,32 @@ type Props = {
 export const DefaultLayout = (props: Props) => {
   return (
     <div className="max-w-xl mx-auto">
-      <header className="bg-teal-200 p-4">
+      <header className="p-4">
         <div className="flex justify-between items-center">
-          <h1 className="w-20">
-            <Link to={"/"}>
+          <h1 className="w-5/12">
+            <Link to={"/"} className="block">
               <img
                 src={`${process.env.PUBLIC_URL}/logo.png`}
                 alt="HairStarロゴ"
               />
             </Link>
           </h1>
-          <div className="cursor-pointer">メニュー</div>
+          <div className="cursor-pointer">アイコン</div>
+        </div>
+        <div>
+          <span>ページ確認用</span>
+          <Link
+            to={"/mypage"}
+            className="inline-block text-blue-900 underline ml-2"
+          >
+            マイページ
+          </Link>
+          <Link
+            to={"/create-post"}
+            className="inline-block text-blue-900 underline ml-2"
+          >
+            募集作成
+          </Link>
         </div>
       </header>
       <main className="bg-slate-100 px-4 py-8">{props.children}</main>
